f089093130487a65860b339dce6c3d0ec5956c52
zhinhz <446011189@qq.com>
2017-06-20 22:29:44 +0800

    1111

--- src/main/java/com/zh/util/pdf/PDFCreateUtil.java
+++ src/main/java/com/zh/util/pdf/PDFCreateUtil.java
@@ -57,10 +57,18 @@ public class PDFCreateUtil {
         cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
         cell.setHorizontalAlignment(align);
         cell.setPhrase(new Phrase(value, font));
-        cell.setBackgroundColor(new BaseColor(235, 235, 235));
+        cell.setBackgroundColor(new BaseColor(255, 255, 255));
         return cell;
     }
 
+    public PdfPCell createBackCell(String value, Font font, int align) {
+        PdfPCell cell = new PdfPCell();
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setHorizontalAlignment(align);
+        cell.setPhrase(new Phrase(value, font));
+        cell.setBackgroundColor(new BaseColor(235, 235, 235));
+        return cell;
+    }
     public PdfPCell createCell(String value, Font font) {
         PdfPCell cell = new PdfPCell();
         cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
@@ -139,16 +147,19 @@ public class PDFCreateUtil {
      * 生成报表
      */
     public void generatePDF( DataResult dataResult) throws Exception {
+        PdfPTable title = createTable(2);
+        title.addCell(createCell("无底柱分段崩落法放矿效果监测系统", headfont, Element.ALIGN_CENTER, 2, false,
+                new BaseColor(255, 255, 255)));
         CkData ckData = dataResult.getCkData();
         PdfPTable table = createTable(2);
-        table.addCell(createCell("无底柱分段崩落法放矿效果监测系统", headfont, Element.ALIGN_CENTER, 2, false,
-                new BaseColor(255, 255, 255)));
+        table.setTotalWidth(280);
+        table.setHorizontalAlignment(Element.ALIGN_LEFT);
         table.addCell(createBlank(10));
 
         table.addCell(createCell("出矿次数汇总", textfont, Element.ALIGN_LEFT, 2, false, null));
-        table.addCell(createCell("总共次数", textfont, Element.ALIGN_CENTER));
+        table.addCell(createBackCell("总共次数", textfont, Element.ALIGN_CENTER));
         table.addCell(createCell(ckData.getTotalNum()+"次", textfont, Element.ALIGN_CENTER));
-        table.addCell(createCell("平均每天工作次数", textfont, Element.ALIGN_CENTER));
+        table.addCell(createBackCell("平均每天工作次数", textfont, Element.ALIGN_CENTER));
         table.addCell(createCell(ckData.getAverNum()+"次", textfont, Element.ALIGN_CENTER));
 
         table.addCell(createBlank(10));
@@ -157,18 +168,18 @@ public class PDFCreateUtil {
         PdfPTable table1 = createTable(6);
         table1.addCell(createCell("时间间隔汇总", textfont, Element.ALIGN_LEFT, 6, false, null));
         table1.addCell(createCell("", textfont, Element.ALIGN_CENTER));
-        table1.addCell(createCell("每次最长时间间隔", textfont, Element.ALIGN_CENTER));
-        table1.addCell(createCell("每次最短时间间隔", textfont, Element.ALIGN_CENTER));
-        table1.addCell(createCell("每天平均最长时间间隔", textfont, Element.ALIGN_CENTER));
-        table1.addCell(createCell("每天平均最短时间间隔", textfont, Element.ALIGN_CENTER));
-        table1.addCell(createCell("总平均时间间隔", textfont, Element.ALIGN_CENTER));
-        table1.addCell(createCell("时间", textfont, Element.ALIGN_CENTER));
+        table1.addCell(createBackCell("每次最长时间间隔", textfont, Element.ALIGN_CENTER));
+        table1.addCell(createBackCell("每次最短时间间隔", textfont, Element.ALIGN_CENTER));
+        table1.addCell(createBackCell("每天平均最长时间间隔", textfont, Element.ALIGN_CENTER));
+        table1.addCell(createBackCell("每天平均最短时间间隔", textfont, Element.ALIGN_CENTER));
+        table1.addCell(createBackCell("总平均时间间隔", textfont, Element.ALIGN_CENTER));
+        table1.addCell(createBackCell("时间", textfont, Element.ALIGN_CENTER));
         table1.addCell(createCell(timeData.getMaxInterTime()+"分钟", textfont, Element.ALIGN_CENTER));
         table1.addCell(createCell(timeData.getMinInterTime()+"分钟", textfont, Element.ALIGN_CENTER));
         table1.addCell(createCell(timeData.getMaxEveryTime()+"分钟", textfont, Element.ALIGN_CENTER));
         table1.addCell(createCell(timeData.getMinEveryTime()+"分钟", textfont, Element.ALIGN_CENTER));
         table1.addCell(createCell(timeData.getTotalAverTime()+"分钟", textfont, Element.ALIGN_CENTER));
-        table1.addCell(createCell("日期", textfont, Element.ALIGN_CENTER));
+        table1.addCell(createBackCell("日期", textfont, Element.ALIGN_CENTER));
         table1.addCell(createCell(timeData.getMaxInterDate(), textfont, Element.ALIGN_CENTER));
         table1.addCell(createCell(timeData.getMinInterDate(), textfont, Element.ALIGN_CENTER));
         table1.addCell(createCell(timeData.getMaxEveryDate(), textfont, Element.ALIGN_CENTER));
@@ -180,11 +191,11 @@ public class PDFCreateUtil {
         List<DetalData.ListData> listData = detalData.getListData();
         PdfPTable table2 = createTable(5);
         table2.addCell(createCell("查询详细结果", textfont, Element.ALIGN_LEFT, 5, false, null));
-        table2.addCell(createCell("序号", textfont, Element.ALIGN_CENTER));
-        table2.addCell(createCell("日期", textfont, Element.ALIGN_CENTER));
-        table2.addCell(createCell("进入时间", textfont, Element.ALIGN_CENTER));
-        table2.addCell(createCell("离开时间", textfont, Element.ALIGN_CENTER));
-        table2.addCell(createCell("间隔时间", textfont, Element.ALIGN_CENTER));
+        table2.addCell(createBackCell("序号", textfont, Element.ALIGN_CENTER));
+        table2.addCell(createBackCell("日期", textfont, Element.ALIGN_CENTER));
+        table2.addCell(createBackCell("进入时间", textfont, Element.ALIGN_CENTER));
+        table2.addCell(createBackCell("离开时间", textfont, Element.ALIGN_CENTER));
+        table2.addCell(createBackCell("间隔时间", textfont, Element.ALIGN_CENTER));
         for (DetalData.ListData listDatum : listData) {
             table2.addCell(createCell(listDatum.getId()+"", textfont, Element.ALIGN_CENTER));
             table2.addCell(createCell(listDatum.getDate(), textfont, Element.ALIGN_CENTER));
@@ -192,6 +203,7 @@ public class PDFCreateUtil {
             table2.addCell(createCell(listDatum.getOutTime(), textfont, Element.ALIGN_CENTER));
             table2.addCell(createCell(listDatum.getInterTime()+"分钟", textfont, Element.ALIGN_CENTER));
         }
+        document.add(title);
         document.add(table);
         document.add(table1);
         document.add(table2);
